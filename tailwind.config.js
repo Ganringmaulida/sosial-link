cba00844b7b14b99e7cb667d371f7976ffe98af5
Ganringmaulida <maulidaganring@gmail.com>
2024-09-10 21:05:36 +0700

    sosial link

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Tambahkan jalur ke file yang berisi kelas Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
